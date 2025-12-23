a40987d383a58ed4891b94d12c3b529b9c790af8
ABDUL AZIZ SAJIB <96626990+AbdulAzizSajib@users.noreply.github.com>
2025-12-23 20:38:47 +0600

    updated
    Refactored Q1 query to include booking details and removed redundant query.

--- queries.sql
+++ queries.sql
@@ -1,16 +1,18 @@
+-- Q1
+SELECT
+  bookings.booking_id,
+  users.name AS customer_name,
+  vehicles.name AS vehicle_name,
+  bookings.start_date,
+  bookings.end_date,
+  bookings.status
+FROM
+  bookings
+  INNER JOIN users ON bookings.user_id = users.user_id
+  INNER JOIN vehicles ON bookings.vehicle_id = vehicles.vehicle_id;
 
 
-SELECT users.name AS customer_name,
-    vehicles.name AS vehicle_name
-FROM bookings
-INNER JOIN users 
-    ON bookings.user_id = users.user_id
-INNER JOIN vehicles 
-    ON bookings.vehicle_id = vehicles.vehicle_id;
-
-
-
-
+-- Q2
 
 SELECT *
 FROM vehicles as v
@@ -20,14 +22,14 @@ WHERE NOT EXISTS (
     WHERE b.vehicle_id = v.vehicle_id
 );
 
-
+-- Q3
 
 SELECT *
 FROM vehicles
 WHERE type = 'car'
   AND status = 'available';
 
-
+--Q4
 
 SELECT vehicles.name AS vehicle_name, COUNT(bookings.booking_id) AS total_bookings
 FROM bookings
